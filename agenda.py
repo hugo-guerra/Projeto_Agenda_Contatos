ac3965fee438229a12be985062c668886023b70a
Hugo Guerra <123105204+hugo-guerra@users.noreply.github.com>
2026-06-21 16:00:56 -0300

    Método editar contato
    Criação do método para a edição dos contatos.

--- agenda.py
+++ agenda.py
@@ -3,8 +3,9 @@ import sqlite3
 class Agenda:
     def __init__(self):
         self.conectar = sqlite3.connect('dados/agenda.db')
+        #LEMBRAR: connect() -> cria uma conexão com o banco de dados.
         self.cursor = self.conectar.cursor()
-
+         #LEMBRAR: cursor() -> cria um cursor, que é o objeto responsável por executar comandos SQL e acessar os resultados.
         self.cursor.execute("""
         CREATE TABLE IF NOT EXISTS Contatos (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -27,15 +28,19 @@ class Agenda:
 
     def listar_contatos(self):
         self.cursor.execute("SELECT * FROM Contatos")
-
-        dados = self.cursor.fetchall()
-
+        #LEMBRAR: execute() -> método do cursor usado para executar um comando SQL no banco de dados SQLite
+        dados = self.cursor.fetchall() 
+        #LEMBRAR: fetchall() -> método usado para buscar todas as linhas retornadas por uma consulta SQL
         return dados 
     
-    def buscar_contatos(self, buscar):
+    def buscar_contatos(self, buscar): 
         self.cursor.execute("SELECT * FROM Contatos WHERE nome = ? OR telefone = ? OR email = ?", (buscar, buscar, buscar))
-
-        dados = self.cursor.fetchall()
-
+        
+        dados = self.cursor.fetchall() 
+        
         return dados
+    
+    def editar_contato(self, id, nome, telefone, email, categoria): #Usando ID pois é a forma mais segura de busca já que ele é PK
+        self.cursor.execute("UPDATE Contatos SET nome = ?, telefone = ?, email = ?, categoria = ? WHERE id = ?", (nome, telefone, email, categoria, id))
 
+        self.conectar.commit()
