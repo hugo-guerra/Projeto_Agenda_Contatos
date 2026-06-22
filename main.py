01d998eea3cca51b45d2c16eacce1aeba02a2b53
Hugo Guerra <123105204+hugo-guerra@users.noreply.github.com>
2026-06-22 11:07:45 -0300

    Main
    Começando a dar forma ao main

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import tkinter as tk
 janela = tk.Tk()
 #LEMBRAR: tk.Tk() -> é a chamada que cria a janela principal da aplicação Tkinter
 janela.title("Minha Janela")
+janela.geometry("600x700")
+
+label = tk.Label(janela, text="Agenda de Contatos")
+label.grid(row=0, column=0)
+
+
 
 janela.mainloop()
-#LEMBRAR: mainloop() -> método que inicia o loop principal de eventos do Tkinter
+#LEMBRAR: mainloop() -> método que inicia o loop principal de eventos do Tkinter, mantém a janela aberta
